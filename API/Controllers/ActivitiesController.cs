01574c8d5bb79e18a0e75fb978e67eae10222ec3
Yuval Beiser <yuval2604@gmail.com>
2020-12-04 12:54:01 +0200

    4.10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -29,5 +29,10 @@ namespace API.Controllers
             return await _mediator.Send(new Detailts.Query{Id = id});
         }
 
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command){
+            return await _mediator.Send(command);
+        }
+
     }
 }
