17557c9ad151f6b040ef7829b36d1f68dbb501d0
Yuval Beiser <yuval2604@gmail.com>
2020-12-05 20:08:06 +0200

    chapter 6 - axios

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import { Item, Button, Label, Segment } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/activity';
+import React, { SyntheticEvent } from "react";
+import { Item, Button, Label, Segment } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
 
 interface IProps {
   activities: IActivity[];
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
+  deleteActivity: (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => void;
+  submitting: boolean;
+  target: string;
 }
 
 const ActivityList: React.FC<IProps> = ({
   activities,
   selectActivity,
-  deleteActivity
+  deleteActivity,
+  submitting,
+  target,
 }) => {
   return (
     <Segment clearing>
       <Item.Group divided>
-        {activities.map(activity => (
+        {activities.map((activity) => (
           <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as='a'>{activity.title}</Item.Header>
+              <Item.Header as="a">{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
@@ -30,15 +37,17 @@ const ActivityList: React.FC<IProps> = ({
               <Item.Extra>
                 <Button
                   onClick={() => selectActivity(activity.id)}
-                  floated='right'
-                  content='View'
-                  color='blue'
+                  floated="right"
+                  content="View"
+                  color="blue"
                 />
                 <Button
-                  onClick={() => deleteActivity(activity.id)}
-                  floated='right'
-                  content='Delete'
-                  color='red'
+                  name={activity.id}
+                  loading={target === activity.id && submitting}
+                  onClick={(e) => deleteActivity(e, activity.id)}
+                  floated="right"
+                  content="Delete"
+                  color="red"
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import reportWebVitals from "./reportWebVitals";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
