aee76f480fdc75a51c762d44f4549f6260b65d9a
Yuval Beiser <yuval2604@gmail.com>
2020-12-05 20:45:22 +0200

    chapeter 7 - axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
-import { Container } from 'semantic-ui-react';
-import { IActivity } from '../models/activity';
-import NavBar from '../../features/nav/NavBar';
-import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import agent from '../api/agent';
-import LoadingComponent from './LoadingComponent';
+import React, { useState, useEffect, Fragment, SyntheticEvent } from "react";
+import { Container } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import NavBar from "../../features/nav/NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -14,62 +14,75 @@ const App = () => {
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
-  const [target, setTarget] = useState('');
+  const [target, setTarget] = useState("");
 
   const handleOpenCreateForm = () => {
     setSelectedActivity(null);
     setEditMode(true);
-  }
+  };
 
   const handleCreateActivity = (activity: IActivity) => {
     setSubmitting(true);
-    agent.Activities.create(activity).then(() => {
-      setActivities([...activities, activity]);
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
-  }
+    agent.Activities.create(activity)
+      .then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
+  };
 
   const handleEditActivity = (activity: IActivity) => {
     setSubmitting(true);
-    agent.Activities.update(activity).then(() => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity])
-      setSelectedActivity(activity);
-      setEditMode(false);
-    }).then(() => setSubmitting(false));
-  }
+    agent.Activities.update(activity)
+      .then(() => {
+        setActivities([
+          ...activities.filter((a) => a.id !== activity.id),
+          activity,
+        ]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
+  };
 
-  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>, id: string) => {
+  const handleDeleteActivity = (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
     setSubmitting(true);
     setTarget(event.currentTarget.name);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(a => a.id !== id)])
-    }).then(() => setSubmitting(false));
-  }
+    agent.Activities.delete(id)
+      .then(() => {
+        setActivities([...activities.filter((a) => a.id !== id)]);
+      })
+      .then(() => setSubmitting(false));
+  };
 
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.filter(a => a.id === id)[0]);
+    setSelectedActivity(activities.filter((a) => a.id === id)[0]);
     setEditMode(false);
   };
 
   useEffect(() => {
     agent.Activities.list()
-      .then(response => {
+      .then((response) => {
         let activities: IActivity[] = [];
         response.forEach((activity) => {
-          activity.date = activity.date.split('.')[0]
+          activity.date = activity.date.split(".")[0];
           activities.push(activity);
-        })
+        });
         setActivities(activities);
-      }).then(() => setLoading(false));
+      })
+      .then(() => setLoading(false));
   }, []);
 
-  if (loading) return <LoadingComponent content='Loading activities' />
+  if (loading) return <LoadingComponent content="Loading activities" />;
 
   return (
     <Fragment>
       <NavBar openCreateForm={handleOpenCreateForm} />
-      <Container style={{ marginTop: '7em' }}>
+      <Container style={{ marginTop: "7em" }}>
         <ActivityDashboard
           activities={activities}
           selectActivity={handleSelectActivity}
