de5230a358b480c373232d6e83a2ce28db96692b
Yuval Beiser <yuval2604@gmail.com>
2020-12-06 12:10:46 +0200

    chapter 7 - mobx - not working

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,32 +1,18 @@
-import React, { SyntheticEvent } from "react";
-import { Item, Button, Label, Segment } from "semantic-ui-react";
-import { IActivity } from "../../../app/models/activity";
+import React, { useContext } from 'react';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-  activities: IActivity[];
-  selectActivity: (id: string) => void;
-  deleteActivity: (
-    event: SyntheticEvent<HTMLButtonElement>,
-    id: string
-  ) => void;
-  submitting: boolean;
-  target: string;
-}
-
-const ActivityList: React.FC<IProps> = ({
-  activities,
-  selectActivity,
-  deleteActivity,
-  submitting,
-  target,
-}) => {
+const ActivityList: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
   return (
     <Segment clearing>
       <Item.Group divided>
-        {activities.map((activity) => (
+        {activitiesByDate.map(activity => (
           <Item key={activity.id}>
             <Item.Content>
-              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Header as='a'>{activity.title}</Item.Header>
               <Item.Meta>{activity.date}</Item.Meta>
               <Item.Description>
                 <div>{activity.description}</div>
@@ -37,17 +23,17 @@ const ActivityList: React.FC<IProps> = ({
               <Item.Extra>
                 <Button
                   onClick={() => selectActivity(activity.id)}
-                  floated="right"
-                  content="View"
-                  color="blue"
+                  floated='right'
+                  content='View'
+                  color='blue'
                 />
                 <Button
                   name={activity.id}
                   loading={target === activity.id && submitting}
                   onClick={(e) => deleteActivity(e, activity.id)}
-                  floated="right"
-                  content="Delete"
-                  color="red"
+                  floated='right'
+                  content='Delete'
+                  color='red'
                 />
                 <Label basic content={activity.category} />
               </Item.Extra>
@@ -59,4 +45,4 @@ const ActivityList: React.FC<IProps> = ({
   );
 };
 
-export default ActivityList;
+export default observer(ActivityList);
