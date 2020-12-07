1c6da995aad0d1c4c2c2ad6bb2c0bd4f3e0cdeb7
Yuval Beiser <yuval2604@gmail.com>
2020-12-07 12:02:25 +0200

    chapter 9 - styling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,36 @@
-import React, { Fragment } from "react";
-import { Container } from "semantic-ui-react";
-import NavBar from "../../features/nav/NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { observer } from "mobx-react-lite";
-import { Route, withRouter, RouteComponentProps } from "react-router-dom";
-import HomePage from "../../features/home/HomePage";
-import ActivityForm from "../../features/activities/form/ActivityForm";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import React, { Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
+import NavBar from '../../features/nav/NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { observer } from 'mobx-react-lite';
+import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+
+
   return (
     <Fragment>
-      <Route exact path="/" component={HomePage} />
-
-      <Fragment>
-        <NavBar />
-        <Container style={{ marginTop: "7em" }}>
-          <Route exact path="/activities" component={ActivityDashboard} />
-          <Route path="/activities/:id" component={ActivityDetails} />
-          <Route
-            key={location.key}
-            path={["/createActivity", "/manage/:id"]}
-            component={ActivityForm}
-          />
-        </Container>
-      </Fragment>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={['/createActivity', '/manage/:id']}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
